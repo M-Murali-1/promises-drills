374ce97c354c33716a3f2bed6329d18cd427f4f5
Murali <murali15062003@gmail.com>
2024-11-18 16:39:28 +0530

    Adding the files

---
+++ exercise-0.js
@@ -0,0 +1,20 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+    5. Print out "Program complete" when the promise completes after 3 seconds
+
+    HINT: Use setTimeout for the delay
+*/
+
+console.log("Program Started");
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve("Program complete");
+    },3000);
+});
+console.log("program is in progress:",promise);
+
+promise.then(result=>console.log(result))
+.catch((error)=>console.log(error));
